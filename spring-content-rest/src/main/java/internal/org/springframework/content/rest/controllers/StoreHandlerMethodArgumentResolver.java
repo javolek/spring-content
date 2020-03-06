a258875f4c92def1ad907aff5a28e0012ccaed9d
Paul Warren <paul.warren@emc.com>
2020-03-05 20:44:17 -0800

    Refactor property resolver
    - temporarily comment out array and collection code that is untested

--- spring-content-rest/src/main/java/internal/org/springframework/content/rest/controllers/StoreHandlerMethodArgumentResolver.java
+++ spring-content-rest/src/main/java/internal/org/springframework/content/rest/controllers/StoreHandlerMethodArgumentResolver.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import javax.persistence.Embeddable;
+import javax.persistence.Entity;
 import javax.servlet.http.HttpServletRequest;
 
 import internal.org.springframework.content.rest.utils.ContentStoreUtils;
@@ -142,119 +143,105 @@ public class StoreHandlerMethodArgumentResolver implements HandlerMethodArgument
         }
 
         PersistentProperty<?> property = getContentPropertyDefinition(entity, contentProperty);
+        Class<?> propertyClass = property.getActualType();
 
-        // get property class
-        Class<?> propertyClass = null;
-        if (!PersistentEntityUtils.isPropertyMultiValued(property)) {
-            propertyClass = property.getActualType();
+        if (isPrimitiveProperty(propertyClass)) {
+
+            ContentStoreInfo info = ContentStoreUtils.findContentStore(stores, domainObj.getClass());
+            if (info == null) {
+                throw new IllegalStateException(String.format("Store for property %s not found", property.getName()));
+            }
+
+            return resolver.resolve(info, domainObj, domainObj, false);
         }
-        // null multi-valued content property
-        else if (PersistentEntityUtils.isPropertyMultiValued(property)) {
-            propertyClass = property.getActualType();
-        }
 
-        Object propVal = null;
-        if (!isPrimitiveProperty(propertyClass)) {
+        // get or create property value
+        PersistentPropertyAccessor accessor = property.getOwner().getPropertyAccessor(domainObj);
+        Object propVal = accessor.getProperty(property);
 
-            // get or create property value
-            PersistentPropertyAccessor accessor = property.getOwner().getPropertyAccessor(domainObj);
-            propVal = accessor.getProperty(property);
-            if (propVal == null) {
-                // null single-valued property
-                if (!PersistentEntityUtils.isPropertyMultiValued(property)) {
-                    propVal = instantiate(propertyClass);
-                    accessor.setProperty(property, propVal);
-                }
-                // null multi-valued content property
-                else {
-                    if (property.isArray()) {
-                        Object member = instantiate(propertyClass);
-                        try {
-                            member = StoreRestController.save(repositories, member);
-                        }
-                        catch (HttpRequestMethodNotSupportedException e) {
-                            e.printStackTrace();
-                        }
+        if (propVal == null) {
 
-                        Object newArray = Array.newInstance(propertyClass, 1);
-                        Array.set(newArray, 0, member);
-                        accessor.setProperty(property, newArray);
-                        propVal = member;
-                    }
-                    else if (property.isCollectionLike()) {
-                        Object member = instantiate(propertyClass);
-                        @SuppressWarnings("unchecked")
-                        Collection<Object> contentCollection = (Collection<Object>) accessor.getProperty(property);
-                        contentCollection.add(member);
-                        propVal = member;
-                    }
-                }
+            if (!PersistentEntityUtils.isPropertyMultiValued(property)) {
+                propVal = instantiate(propertyClass);
+                accessor.setProperty(property, propVal);
             }
             else {
-                if (contentPropertyId != null) {
-                    if (property.isArray()) {
-                        Object componentValue = null;
-                        for (Object content : (Object[]) propVal) {
-                            if (BeanUtils.hasFieldWithAnnotation(content, ContentId.class) && BeanUtils
-                                    .getFieldWithAnnotation(content, ContentId.class) != null) {
-                                String candidateId = BeanUtils.getFieldWithAnnotation(content, ContentId.class)
-                                        .toString();
-                                if (candidateId.equals(contentPropertyId)) {
-                                    componentValue = content;
-                                    break;
-                                }
-                            }
-                        }
-                        propVal = componentValue;
-                    }
-                    else if (property.isCollectionLike()) {
-                        Object componentValue = null;
-                        for (Object content : (Collection<?>) propVal) {
-                            if (BeanUtils.hasFieldWithAnnotation(content, ContentId.class) && BeanUtils
-                                    .getFieldWithAnnotation(content, ContentId.class) != null) {
-                                String candidateId = BeanUtils.getFieldWithAnnotation(content, ContentId.class)
-                                        .toString();
-                                if (candidateId.equals(contentPropertyId)) {
-                                    componentValue = content;
-                                    break;
-                                }
-                            }
-                        }
-                        propVal = componentValue;
-                    }
-                }
-                else if (contentPropertyId == null &&
-                        (PersistentEntityUtils.isPropertyMultiValued(property) &&
-                                (method.equals(HttpMethod.GET) || method.equals(HttpMethod.DELETE)))) {
-                    throw new MethodNotAllowedException("GET", null);
-                }
-                else if (contentPropertyId == null) {
-                    if (property.isArray()) {
-                        Object member = instantiate(propertyClass);
-                        Object newArray = Array.newInstance(propertyClass, Array.getLength(propVal) + 1);
-                        System.arraycopy(propVal, 0, newArray, 0, Array.getLength(propVal));
-                        Array.set(newArray, Array.getLength(propVal), member);
-                        accessor.setProperty(property, newArray);
-                        propVal = member;
-                    }
-                    else if (property.isCollectionLike()) {
-                        Object member = instantiate(propertyClass);
-                        @SuppressWarnings("unchecked")
-                        Collection<Object> contentCollection = (Collection<Object>) accessor.getProperty(property);
-                        contentCollection.add(member);
-                        propVal = member;
-                    }
-                }
+//                if (property.isArray()) {
+//                    Object member = instantiate(propertyClass);
+//                    try {
+//                        member = StoreRestController.save(repositories, member);
+//                    }
+//                    catch (HttpRequestMethodNotSupportedException e) {
+//                        e.printStackTrace();
+//                    }
+//
+//                    Object newArray = Array.newInstance(propertyClass, 1);
+//                    Array.set(newArray, 0, member);
+//                    accessor.setProperty(property, newArray);
+//                    propVal = member;
+//                } else if (property.isCollectionLike()) {
+//                    Object member = instantiate(propertyClass);
+//                    @SuppressWarnings("unchecked")
+//                    Collection<Object> contentCollection = (Collection<Object>) accessor.getProperty(property);
+//                    contentCollection.add(member);
+//                    propVal = member;
+//                }
             }
         } else {
-            propVal = domainObj;
+            if (isCollectionElementRequest(contentPropertyId)) {
+                if (property.isArray()) {
+//                    Object componentValue = null;
+//                    for (Object content : (Object[]) propVal) {
+//                        if (BeanUtils.hasFieldWithAnnotation(content, ContentId.class) &&
+//                            BeanUtils.getFieldWithAnnotation(content, ContentId.class) != null) {
+//                            String candidateId = BeanUtils.getFieldWithAnnotation(content, ContentId.class).toString();
+//                            if (candidateId.equals(contentPropertyId)) {
+//                                componentValue = content;
+//                                break;
+//                            }
+//                        }
+//                    }
+//                    propVal = componentValue;
+                } else if (property.isCollectionLike()) {
+//                    Object componentValue = null;
+//                    for (Object content : (Collection<?>) propVal) {
+//                        if (BeanUtils.hasFieldWithAnnotation(content, ContentId.class) && BeanUtils
+//                                .getFieldWithAnnotation(content, ContentId.class) != null) {
+//                            String candidateId = BeanUtils.getFieldWithAnnotation(content, ContentId.class)
+//                                    .toString();
+//                            if (candidateId.equals(contentPropertyId)) {
+//                                componentValue = content;
+//                                break;
+//                            }
+//                        }
+//                    }
+//                    propVal = componentValue;
+                }
+            } else if (isCollectionRequest(contentPropertyId) &&
+                    (PersistentEntityUtils.isPropertyMultiValued(property) &&
+                            (method.equals(HttpMethod.GET) || method.equals(HttpMethod.DELETE)))) {
+                throw new MethodNotAllowedException("GET", null);
+            } else if (isCollectionRequest(contentPropertyId) ) {
+                if (property.isArray()) {
+//                    Object member = instantiate(propertyClass);
+//                    Object newArray = Array.newInstance(propertyClass, Array.getLength(propVal) + 1);
+//                    System.arraycopy(propVal, 0, newArray, 0, Array.getLength(propVal));
+//                    Array.set(newArray, Array.getLength(propVal), member);
+//                    accessor.setProperty(property, newArray);
+//                    propVal = member;
+                }
+                else if (property.isCollectionLike()) {
+                    Object member = instantiate(propertyClass);
+                    @SuppressWarnings("unchecked")
+                    Collection<Object> contentCollection = (Collection<Object>) accessor.getProperty(property);
+                    contentCollection.add(member);
+                    propVal = member;
+                }
+            }
         }
 
         // get property store
         ContentStoreInfo info = ContentStoreUtils.findContentStore(stores, propertyClass);
-        if (info == null) {
-            info = ContentStoreUtils.findContentStore(stores, domainObj.getClass());
-        }
         if (info == null) {
             throw new IllegalStateException(String.format("Store for property %s not found", property.getName()));
         }
@@ -265,6 +252,14 @@ public class StoreHandlerMethodArgumentResolver implements HandlerMethodArgument
         }
 
         return resolver.resolve(info, domainObj, propVal, embeddedProperty);
+    }
+
+    private boolean isCollectionElementRequest(String contentPropertyId) {
+        return contentPropertyId != null;
+    }
+
+    private boolean isCollectionRequest(String contentPropertyId) {
+        return contentPropertyId == null;
     }
 
     private PersistentProperty<?> getContentPropertyDefinition(PersistentEntity<?, ?> persistentEntity, String contentProperty) {
